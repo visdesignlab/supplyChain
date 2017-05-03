ac8b18b813c9ab79734182bf639e7f26453813bb
cnobre <carolinanobre@gmail.com>
2017-05-03 16:22:27 -0600

    added files to gitignore

--- src/language.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by cnobre on Wed, 03 May 2017 17:48:48 GMT.
- */
-/**
- * Created by cnobre on Wed, 03 May 2017 17:48:48 GMT.
- */ export var APP_NAME = 'supplyChain';
-export var HELLO_WORLD = 'Hello Phovea World';
-//# sourceMappingURL=language.js.map
